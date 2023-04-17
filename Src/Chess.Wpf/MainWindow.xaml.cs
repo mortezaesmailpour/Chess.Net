1758750344245748fc5205613b84f9c5dc98b4f6
Morteza Esmailpour <Morteza@Esmailpour.Net>
2023-04-18 02:00:55 +0300

    View was added to MainWindow

--- Src/Chess.Wpf/MainWindow.xaml.cs
+++ Src/Chess.Wpf/MainWindow.xaml.cs
@@ -1,28 +1,29 @@
-﻿using System;
+﻿using Chess.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Chess.Wpf
+namespace Chess.Wpf;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public BoardViewModel Board { get; set; }
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        DataContext = this;
+        Board = new BoardViewModel();
+    }
+
+    private void Square_Click(object sender, MouseButtonEventArgs e)
+    {
+        var image = (Image)sender;
+        var square = (SquareViewModel)image.DataContext;
+        Board.HandleClick(square);
     }
 }
