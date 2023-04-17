c901d4269dc08a5b83736b9a15986d3eebcc6a40
Morteza Esmailpour <Morteza@Esmailpour.Net>
2023-04-18 01:44:32 +0300

    viewmodels was added

---
+++ Src/Chess/ViewModel/CommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace Chess.ViewModel;
+internal class CommandHandler : ICommand
+{
+    private readonly Action _action;
+
+    public CommandHandler(Action action)
+    {
+        _action = action;
+    }
+    public bool CanExecute(object? parameter) => true;
+    public void Execute(object? parameter) => _action();
+
+    public event EventHandler CanExecuteChanged;
+}
