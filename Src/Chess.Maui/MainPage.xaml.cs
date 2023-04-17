bd75b39c8c816ab03aa52b889c581b0a9218c5ab
Morteza Esmailpour <Morteza@Esmailpour.Net>
2023-04-18 01:53:00 +0300

    View was added to MainPage

--- Src/Chess.Maui/MainPage.xaml.cs
+++ Src/Chess.Maui/MainPage.xaml.cs
@@ -1,24 +1,15 @@
-﻿namespace Chess.Maui
+﻿using Chess.ViewModel;
+
+namespace Chess.Maui
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        public BoardViewModel Board { get; set; }
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Board = new BoardViewModel();
+            BindingContext = this;
         }
     }
 }
